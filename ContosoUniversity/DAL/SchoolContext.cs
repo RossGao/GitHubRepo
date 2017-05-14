6f475a19b40874ed3086037c20b247eb59d3f01c
高翔 <gaoxiang139@aliyun.com>
2017-05-14 23:32:29 +0800

    Use migration to change database schema.

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -18,6 +18,11 @@ namespace ContosoUniversity.DAL
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+                .Map(t => t.MapLeftKey("CourseID")
+                .MapRightKey("InstructorID")
+                .ToTable("CourseInstructor"));
         }
     }
 }
